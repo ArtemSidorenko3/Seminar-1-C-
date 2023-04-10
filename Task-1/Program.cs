8014865c72c1c66a3980cdc1ed5f44c9c2b2ba10
Artem_Sidorenko <artem70947@gmail.com>
2023-04-10 14:48:54 +0300

    Задача 1 квадрат числа

---
+++ Task-1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число и выводит его квадрат(число умноженное на само себя).
+
+Console.Write("Введите число: ");
+
+int  a = int.Parse(Console.ReadLine());
+int b = a * a;
+
+Console.Write("Ответ: ");
+Console.Write(a * a);
