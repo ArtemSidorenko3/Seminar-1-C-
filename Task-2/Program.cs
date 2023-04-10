fc8c3f951a12f12e8bfb1ff6a308ee1c0cc50b77
Artem_Sidorenko <artem70947@gmail.com>
2023-04-10 15:11:42 +0300

    Задача 2, Операции bool

---
+++ Task-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите число: ");
+
+ int number1 = int.Parse(Console.ReadLine());
+ int number2 = int.Parse(Console.ReadLine());
+
+if(number1 == Math.Pow(number2, 2))
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
