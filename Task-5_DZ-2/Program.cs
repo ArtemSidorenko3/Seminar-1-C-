882c9d7244049c7873c960b61b9cb398d54939b8
Artem_Sidorenko <artem70947@gmail.com>
2023-04-12 14:20:42 +0300

    Задача 5, Домашнее задание №5 решение

--- Task-5_DZ-2/Program.cs
+++ Task-5_DZ-2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.WriteLine("Ввудите число 1: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 3: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Максимальное число: " + Math.Max(a, Math.Max(b,c)));
