00908bb6cd32efab01a9858851cacc1787df6440
Artem_Sidorenko <artem70947@gmail.com>
2023-04-12 15:00:13 +0300

    Задача 9, Домашнее задание №4 решение

---
+++ Task-9_DZ-4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число(N), а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= a; i += 2)
+{
+    if (i < a - 1)
+    Console.WriteLine(i);
+
+    else
+    Console.WriteLine (i);
+}
