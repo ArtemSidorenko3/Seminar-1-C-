f7a44e70700318e5e18d389d8ef5ea7cd3465a16
Artem_Sidorenko <artem70947@gmail.com>
2023-04-12 14:39:09 +0300

    Задача 7, Домашнее задание

--- Task-7_Dz-3/Program.cs
+++ Task-7_Dz-3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+double a = double.Parse(Console.ReadLine());
+ double b = a % 2;
+ if(b)
+ {
+    Console.WriteLine("Четное a число: ");
+    Console.WriteLine("Нечетное a число: ");
+ }
