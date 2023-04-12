c234bd53a782e1c4f4538a55cdbda1afcfc43a2a
Artem_Sidorenko <artem70947@gmail.com>
2023-04-12 14:04:30 +0300

    Задача 3, Домашнее задание №1 решение

--- Task-3_DZ-1/Program.cs
+++ Task-3_DZ-1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ввудите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Максимальное число: " + Math.Max(a,b));
+
+Console.WriteLine("Минимальное число: " + Math.Min(a,b));
