367b60fe2582328d1d4ecb31e3cc49eb67c70b7d
Artem_Sidorenko <artem70947@gmail.com>
2023-04-21 15:14:04 +0300

    Решение задачи №14

---
+++ Task-13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно оодновременно 7 и 23.
+
+/*Console.WriteLine ("Введите число 1: ");
+
+int a = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите число 2: ");
+
+int b = int.Parse (Console.ReadLine());
+
+
+if (a % 7 == 0 & b % 23 == 0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine("Не кратно");
+}*/
+
+
+
+// Напишите программу, вычисляющую площадь круга
+
+System.Console.WriteLine("Введите радиус круга: ");
+
+int radius = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Площадь круга = " + Math.PI * Math.Pow(radius, 2));
