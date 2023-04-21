1a3114df6018985eee1401a8320eec8dcae0afd3
Artem_Sidorenko <artem70947@gmail.com>
2023-04-21 15:27:21 +0300

    Решение задачи №16

---
+++ Task-14/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+System.Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a * a == b || b * b ==a)
+{
+System.Console.WriteLine("Является квадратом одного числа к другому!");
+}
+else
+{
+System.Console.WriteLine("Не является квадратом одного числа к другому!");
+}
